1c8031f2a161f3a0769509bdfd4ff1378885f333
EC2 Default User <ec2-user@ip-172-31-4-19.us-east-2.compute.internal>
2024-02-08 23:17:00 +0000

    working api

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acmedb');
+const express = require('express');
+const app = express();
 
+app.get('/', (req, res, next)=> {
+    res.send('hello world');
+});
+
+app.get('/api/flavors', async(req, res, next)=> {
+    try {
+        const SQL = `
+            SELECT *
+            FROM flavors
+        `;
+        const response = await client.query(SQL);
+        res.send(response.rows);
+    }
+    catch(ex){
+        next(ex);
+    }
+});
 
 const init = async()=> {
+    
     await client.connect();
     console.log('connected to database');
     let SQL = `
@@ -23,6 +43,9 @@ const init = async()=> {
     `;
     await client.query(SQL);
     console.log('data seeded');
+    
+    const port = process.env.PORT || 3000;
+    app.listen(port, ()=> console.log(`listening on port ${port}`));
 };
 
 init();
