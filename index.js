8989fc29f2df44d3407cbafa53d98d987300962b
EC2 Default User <ec2-user@ip-172-31-4-19.us-east-2.compute.internal>
2024-02-08 19:46:17 +0000

    added flavors table

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('hello world')
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acmedb');
+
+
+const init = async()=> {
+    await client.connect();
+    console.log('connected to database');
+    let SQL = `
+        DROP TABLE IF EXISTS flavors;
+        CREATE TABLE flavors(
+            id SERIAL PRIMARY KEY,
+            txt VARCHAR(100),
+            ranking INTEGER DEFAULT 5,
+            created_at TIMESTAMP DEFAULT now()
+        );
+    `;
+    await client.query(SQL);
+    console.log('table created');
+    SQL = `
+        INSERT INTO flavors(txt) VALUES ('chocolate');
+        INSERT INTO flavors(txt) VALUES ('vanilla');
+        INSERT INTO flavors(txt) VALUES ('strawberry');
+    `;
+    await client.query(SQL);
+    console.log('data seeded');
+};
+
+init();
